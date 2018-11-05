0969ed5fcbbd3dde0e0f71ec91cf6462be90fbc5
Sarah <sb1401920@qu.edu.qa>
2018-11-05 14:35:23 +0300

    working on it

--- TestApp/app/src/main/java/com/example/boo/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/boo/testapp/MainActivity.java
@@ -7,11 +7,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    int[] IMAGES = {R.drawable.applebees, R.drawable.pullbear, R.drawable.paul};
+    String[] OFFERS = {"BUY ONE GET ONE!", "UP TO 50% OFF!" , "SALES UPTO 70%"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+        ListView listview = (ListView) findViewById(R.id.Offers_ListView);
+
+        CustomAdapter customAdapter = new MainActivity.CustomAdapter();
+        listview.setAdapter(customAdapter);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -39,5 +51,39 @@ public class MainActivity extends AppCompatActivity {
     public void clickButton(View v){
         Intent i = new Intent(this, Categories.class);
         startActivity(i);
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return IMAGES.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.offers_layout, null);
+
+            ImageView imageView = (ImageView) view.findViewById(R.id.Offers_imageView);
+            TextView textViewTitle = (TextView) view.findViewById(R.id.Offers_TextView);
+            ImageView imageView2 = (ImageView) view.findViewById(R.id.Offers_imageView2);
+            TextView textViewTitle2 = (TextView) view.findViewById(R.id.Offers_TextView2);
+
+            imageView.setImageResource(IMAGES[i]);
+            textViewTitle.setText(OFFERS[i]);
+            imageView2.setImageResource(IMAGES[i]);
+            textViewTitle2.setText(OFFERS[i]);
+            return view;
+        }
     }
 }
